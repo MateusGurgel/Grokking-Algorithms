9ddc8240d0f9319955ebe098d29b34b2dd734df1
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-03-20 20:34:41 -0300

    initial commit

---
+++ Greedy Algorithms/GreedyAlgorithms.py
@@ -0,0 +1,49 @@
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfor"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+def get_best_station(coveredStates):
+    bestStation = None
+    bestStationCovered = float('inf')
+     
+    for station in stations:
+        statesAreadyCovered = len(stations[station] & coveredStates)
+
+        if (statesAreadyCovered < bestStationCovered):
+            bestStation = station
+            bestStationCovered = statesAreadyCovered
+
+    return bestStation
+        
+def get_best_states(numberOfStations):
+
+    bestStates = set()
+
+    for i in range(numberOfStations):
+        bestStation = get_best_station(bestStates)
+
+        for state in stations[bestStation]:
+            bestStates.add(state)
+    
+    return bestStates
+
+def get_best_stations(numberOfStations):
+
+    bestStations = set()
+    bestStates = set()
+
+    for i in range(numberOfStations):
+
+        bestStation = get_best_station(bestStates)
+        bestStations.add(bestStation)
+
+        for state in stations[bestStation]:
+            bestStates.add(state)
+    
+    return bestStations
+
+print(get_best_states(4))
+print(get_best_stations(4))
